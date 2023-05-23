370832cbf7af9ed9aed4e54535210be3dd538be9
AndreiSaramet <andreisaramet01@yahoo.com>
2023-05-23 22:26:11 +0300

    test pages

--- test/src/test/java/org/ubb/cs/test/pages/MainPage.java
+++ test/src/test/java/org/ubb/cs/test/pages/MainPage.java
@@ -2,7 +2,12 @@ package org.ubb.cs.test.pages;
 
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.By;
 
 @DefaultUrl("http://localhost:4200")
 public class MainPage extends PageObject {
+    public void log_in() {
+        this.find(By.cssSelector(".user-icon")).click();
+        this.find(By.cssSelector(".mat-mdc-menu-item")).click();
+    }
 }
